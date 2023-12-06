7ee20ed1f2889250eb3fea55a4573675bb2e0a57
katlegomaphango <mphangokatlego08@gmail.com>
2023-12-06 14:47:22 +0200

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,141 @@
+import { MenuOutlined, Camera, Instagram, Twitter, Facebook, Pinterest } from "@mui/icons-material"
+import { AppBar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Typography, styled, Button } from "@mui/material"
+import { useState } from "react"
+
+const PreText = styled(Typography)({
+    
+})
+const PostText = styled(Typography)({
+
+})
+
+const pages = ['About', 'Gallery', 'Exhibitions', 'Contact']
 
 const Navbar = () => {
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        <div>Navbar</div>
+        <>
+            <AppBar position="static" color="transparent">
+                <Container maxWidth="lg">
+                    <Toolbar disableGutters>
+                        <Camera sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            }}
+                        >
+                            <PreText>ROCI</PreText>
+                            <PostText>NANTE</PostText>
+                        </Typography>
+
+                        {/* Menu */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuOutlined />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <Camera sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            }}
+                        >
+                            <PreText>ROCI</PreText>
+                            <PostText>NANTE</PostText>
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+                            {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'burlywood', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex'}, justifyContent: 'flex-end'}}>
+                            <IconButton>
+                                <Instagram />
+                            </IconButton>
+                            <IconButton>
+                                <Twitter />
+                            </IconButton>
+                            <IconButton>
+                                <Facebook />
+                            </IconButton>
+                            <IconButton>
+                                <Pinterest />
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            Navbar
+        </>
     )
 }
 
