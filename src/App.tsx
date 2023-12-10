e155e4b81fe951c50d106906a214df24a615600a
katlegomaphango <mphangokatlego08@gmail.com>
2023-12-10 19:02:33 +0200

    unsplash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
 import { Navbar } from "./components"
+import { createApi } from "unsplash-js";
+
+type Photo = {
+  id: number;
+  width: number;
+  height: number;
+  urls: { large: string; regular: string; raw: string; small: string };
+  color: string | null;
+  user: {
+    username: string;
+    name: string;
+  };
+};
+
+const api = createApi({
+  // Don't forget to set your access token here!
+  // See https://unsplash.com/developers
+  accessKey: import.meta.env.VITE_CLIENT_ID
+});
 
 const App = () => {
   return (
--- src/redux/services/unsplash.ts
+++ src/redux/services/unsplash.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const unsplashAPI = createApi({
     reducerPath: 'unsplashAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.unsplash.com/'
+        baseUrl: 'https://api.unsplash.com/',
+        credentials: 'include',
+        
     }),
     endpoints: (builder) => ({
         getRandomHeroImage: builder.query({query: () => '/'})
