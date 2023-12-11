4b84e944957469e04f8fd0d5da38a2804cf54801
katlegomaphango <mphangokatlego08@gmail.com>
2023-12-11 23:32:08 +0200

    refract

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { Navbar } from "./components"
-import { useGetRandomPhotoQuery } from "./redux/services/unsplash";
-
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+import { Hero, Navbar } from "./components"
 
 const App = () => {
-  const { data, error } = useGetRandomPhotoQuery({ clientID: CLIENT_ID, count: 8})
-  if(error) throw error
-  console.log(data)
+  
 
   return (
     <>
       <Navbar />
-      App
-      {data && <img src={data[0].urls.regular} />}
+      <Hero />
+      
+      
     </>
   )
 }
