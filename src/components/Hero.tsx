def2778523e7d77f7edfae90bfd0457d5867315c
katlegomaphango <mphangokatlego08@gmail.com>
2023-12-12 23:24:50 +0200

    simpliefied

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,39 +3,9 @@ import { CLIENT_ID, Photo } from "../constants"
 import { useEffect, useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 
-const slideVariants = {
-    hiddenRight: {
-        x: "100%",
-        opacity: 0,
-    },
-    hiddenLeft: {
-        x: "-100%",
-        opacity: 0,
-    },
-    visible: {
-        x: "0",
-        opacity: 1,
-        transition: {
-            duration: 1,
-        }
-    },
-    hover: {
-        scale: 1.2,
-        backgroundColor: "#ff00008e",
-    },
-    exit: {
-        opacity: 0,
-        scale: 0.8,
-        transition: {
-            duration: 0.5,
-        }
-    }
-}
-
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [direction, setDirection] = useState('left')
     
     const { data, error } = useGetRandomPhotoQuery({ clientID: CLIENT_ID, count: 5})
     const images: Photo[] = data
@@ -45,7 +15,6 @@ const Hero = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prev) => prev + 1 === 5 ? currentIndex - 4 : currentIndex + 1);
-            setDirection((prev) => prev === 'right' ? 'left' : 'right')
         }, 2000);
 
         return () => {
@@ -63,10 +32,6 @@ const Hero = () => {
                         <motion.div 
                             key={currentIndex} 
                             style={{backgroundImage: `url("${images[currentIndex].urls.regular}")`}}
-                            variants={slideVariants}
-                            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
-                            animate="visible"
-                            exit="exit"
                             className="div-img"
                         ></motion.div>
                     </AnimatePresence>
